b9e526e5cb5139e121f32d5d12e02cd264c938c2
Liz Lorena <31869677+lizberberena@users.noreply.github.com>
2023-08-19 21:44:43 -0400

    Update Blackjack.java

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -17,7 +17,7 @@ public class Blackjack {
     System.out.println(" _/_/    _/    _/    _/_/_/  _/    _/       ");
     System.out.println("\n\n");
     
-    System.out.println("\n@irisoflys on Github");
+    System.out.println("\nliz lorena berberena");
     System.out.println("ascii font lean from http://www.network-science.de/ascii/");
     Deck deck = new Deck();
     System.out.println("\nType Y for Instructions or any other character to continue");
